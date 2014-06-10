3b20837458cf521e975ac77e0166d07d23d78bb3
StevenWungMac <cyberhero@yeah.net>
2014-06-10 23:18:09 +0800

    updated

--- phototips/phototips.php
+++ phototips/phototips.php
@@ -20,8 +20,26 @@
     
     define("ERROR_CODE_NO_TOKEN", 0);
 
-    $header = "<style type='text/css'>img{width: 100%;}</style>";
-    $header.= "<script>function loaded(){window.location = 'loaded://'; }</script><body onload='loaded();'>";
+    $header = "<html><head><style type='text/css'>
+    			.ft-body img{width: 100%; height: auto} 
+    			.ft-body{font-size:16px;padding-top:2px;} 
+    			h2,p,strong{font-size:16px;font-weight:normal!important;}
+    			img{margin:2px 0px;}
+    			
+    		   </style>";
+	$header.= "<script src='m/file/jquery.js'></script>
+			   <script src='m/file/jquery.lazyload.js'></script>";
+			   
+	$header.= " <script>
+					$(function(){
+						$('img').lazyload({effect:'fadeIn'});
+					});
+				</script>
+			  ";
+
+    $header.= "<script>function loaded(){window.location = 'loaded://'; }</script></head><body onload='loaded();'>";
+    
+    $footer = "</body></html>";
 
     require_once 'api_libs.php';
 
@@ -39,10 +57,22 @@
         ),
         'comment/(?P<post_id>[0-9]+)/(?P<timestamp>[0-9]+)/(?P<count>[0-9]+)/?' => array(
             array( 'ft_get_comments',  GET)
-        )
+        ),
+        'file/(?P<file>.*)' => array(
+            array( 'ft_get_file',  GET)
+        ),
+         
     );
-
+    function ft_get_file($file){
+    	$cpath = realpath( dirname(__FILE__) );
+    	//header('Content-Type: application/x-javascript');
+    	echo file_get_contents($cpath.'/files/'.$file);
+    	die();
+	}
+	
     function ft_get_comments( $post_id, $timestamp , $count ){
+    	global $header;
+    	/*
         $real_count = 0;
         $comments = array();
         $raw_comments = get_comments( array(
@@ -72,11 +102,52 @@
         $result['data'] = $comments;
 
         echo json_encode($result);
+        */
+        
+        $comment = $header;
+		$raw_comments = get_comments( array(
+            'post_id' => $post_id,
+            'author_email' => FT_COMMENT_AUTHOR_EMAIL,
+            'number' => $count,
+            'offset' => $timestamp,
+        ));
+        $comment.= "<div style='border-top:1px #ccc dashed'>";
+        foreach ($raw_comments as $k=> $raw_comment){
+            $timestr = $raw_comment->comment_date;
+            $content = $raw_comment->comment_content;
+            
+            $date = date('Y-m-d', strtotime($timestr));;
+            $author = substr($content, 0, strpos($content, ':'));
+            $content = substr($content, strpos($content, ':') + 1 );
+           
+           
+            $item = "	<div style='border-bottom:1px #ccc dashed'>";
+            $item.= "		<div style='padding:3px 0 1px 0;color:#666;font-size:14px;'>";
+            $item.= "			<span style='display:inline-block;'>$author</span>";
+            $item.= "			<span style='display:inline-block;float:right;padding-right:5px;'>$date</span>";
+            $item.= "		</div>";
+            $item.= "		<div style='padding:7px 0 7px 0'>$content</div>";
+            $item.= "	</div>";
+            /*
+            $comment = array();
+            $comment['id'] = $raw_comment->comment_ID;
+            $comment['date'] = date('Y-m-d', strtotime($timestr));
+            //$comment['timestamp'] = strval(strtotime($timestr));
+            $comment['timestamp'] = strval($k + 1);
+            $comment['author'] = substr($content, 0, strpos($content, ':'));
+            $comment['comment'] = substr($content, strpos($content, ':') + 1 );
+            $comments[] = $comment;
+            */
+            $comment.= $item;
+        }
+        $comment.= "</div>";
+        
+        echo $comment;
         die();
     }
     function ft_post_comments($post_id){
         if( !check_token() ){
-            post_error(ERROR_CODE_NO_TOKEN, "token is not right");
+            //post_error(ERROR_CODE_NO_TOKEN, "token is not right");
         }
         global $header;
         $comment = $_POST['comment'];
@@ -87,7 +158,7 @@
         if($comment == ''){
             return;
         }
-
+		 
         $_SERVER['REQUEST_METHOD'] = 'POST';
         $_POST['comment_post_ID'] = $post_id;;
         $_POST['author'] = FT_COMMENT_AUTHOR_NAME;
@@ -102,7 +173,22 @@
         global $header;
         query_posts("p=$id");
         the_post();
-        echo $header . get_the_content();
+        $post = get_the_content();
+        $post = str_replace('src=',  "src='m/file/grey.gif' data-original=", $post);
+        //die($post);
+        
+        $content = $header;
+        $content.= "<div style='font-size:18px;font-weight:bold;border-bottom:#ccc 1px dashed;padding:8px 0px'>".get_the_title()."</div>";
+		$content.= "<div style='padding-top:5px;'>";
+		$content.= "	<div style='width:100px;height:20px;display:inline-block;float:left;'>2011-12-22</div>";
+		$content.= "	<div style='width:100px;height:20px;display:inline-block;float:left;'><a href='comment:/'>100 comments</a></div>";
+		$content.= "</div>";
+		$content.= "<div style='clear:both'></div>";		
+		$content.= "<div class='ft-body'>";		
+        $content.= $post;
+        $content.= "</div>";
+        $content.= $footer;
+        echo $content;
         die();
     }
 
